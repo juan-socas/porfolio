7e1f811e5dce3dcee0f50121490c4192c6ce2532
JuanS <sandbox1shack@proton.me>
2026-05-11 23:24:36 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import robotsTxt from "astro-robots-txt"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://juan-socas.github.io'
+  site: 'https://juan-socas.github.io',
   base:'/porfolio'
 })
